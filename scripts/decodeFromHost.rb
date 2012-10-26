b260eac0af6b47c037c07f22d6e78a7ac91b6c90
Janosch Machowinski <janosch.machowinski@dfki.de>
2012-10-26 17:40:42 +0200

    added script for decoding a stream to images

---
+++ scripts/decodeFromHost.rb
@@ -0,0 +1,32 @@
+#! /usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require 'orocos'
+include Orocos
+require 'readline'
+
+
+if !ARGV[0]  then 
+    puts "usage: hostname/ip"
+    exit
+end
+Orocos::CORBA.name_service = ARGV[0]
+Orocos::CORBA::max_message_size = 100000000
+Orocos.initialize
+
+# setup the environment so that ruby can find the test deployment
+ENV['PKG_CONFIG_PATH'] = "#{File.expand_path("..", File.dirname(__FILE__))}/build:#{ENV['PKG_CONFIG_PATH']}"
+
+Orocos.run('video_streamer::VideoDecoder' => 'decoder', :valgrind => false) do
+    encoder = TaskContext.get('encoder')
+    decoder = TaskContext.get('decoder')
+
+    encoder.mpeg_stream.connect_to(decoder.mpeg_stream, :type => :buffer, :size => 1000 )
+
+    decoder.configure()
+    decoder.start()
+    
+    Readline.readline()
+    
+    STDERR.puts "shutting down"
+end
